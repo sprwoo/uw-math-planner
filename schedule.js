8ad27a3db478c6499b763af77f1924b2d5435f79
Ajai Chandi <ajaischandi@gmail.com>
2024-04-28 16:09:50 -0400

    Changed tags, added max 2 feature

--- schedule.js
+++ schedule.js
@@ -1,47 +1,35 @@
-/* ===== Logic for creating fake Select Boxes ===== */
-$('.sel').each(function() {
-    $(this).children('select').css('display', 'none');
-    
-    var $current = $(this);
-    
-    $(this).find('option').each(function(i) {
-      if (i == 0) {
-        $current.prepend($('<div>', {
-          class: $current.attr('class').replace(/sel/g, 'sel__box')
-        }));
+document.addEventListener('DOMContentLoaded', function() {
+    var checks = document.querySelectorAll('input[name="majors"]');
+    var max = 2;
+
+    for (var i = 0; i < checks.length; i++) {
+        checks[i].onclick = selectiveCheck;
+    }
+
+    function selectiveCheck(event) {
+        var checkedChecks = document.querySelectorAll('input[name="majors"]:checked');
         
-        var placeholder = $(this).text();
-        $current.prepend($('<span>', {
-          class: $current.attr('class').replace(/sel/g, 'sel__placeholder'),
-          text: placeholder,
-          'data-placeholder': placeholder
-        }));
+        if (checkedChecks.length >= max + 1) {
+            event.preventDefault(); // Prevent further checks if limit is reached
+            return false;
+        }
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var checks = document.querySelectorAll('input[name="minors"]');
+    var max = 2;
+
+    for (var i = 0; i < checks.length; i++) {
+        checks[i].onclick = selectiveCheck;
+    }
+
+    function selectiveCheck(event) {
+        var checkedChecks = document.querySelectorAll('input[name="minors"]:checked');
         
-        return;
-      }
-      
-      $current.children('div').append($('<span>', {
-        class: $current.attr('class').replace(/sel/g, 'sel__box__options'),
-        text: $(this).text()
-      }));
-    });
-  });
-  
-  // Toggling the `.active` state on the `.sel`.
-  $('.sel').click(function() {
-    $(this).toggleClass('active');
-  });
-  
-  // Toggling the `.selected` state on the options.
-  $('.sel__box__options').click(function() {
-    var txt = $(this).text();
-    var index = $(this).index();
-    
-    $(this).siblings('.sel__box__options').removeClass('selected');
-    $(this).addClass('selected');
-    
-    var $currentSel = $(this).closest('.sel');
-    $currentSel.children('.sel__placeholder').text(txt);
-    $currentSel.children('select').prop('selectedIndex', index + 1);
-  });
-  
+        if (checkedChecks.length >= max + 1) {
+            event.preventDefault(); // Prevent further checks if limit is reached
+            return false;
+        }
+    }
+});

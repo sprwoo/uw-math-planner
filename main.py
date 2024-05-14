f286c7fba16b345ac7c64f08c433db7b00ea0e50
Daniel Yang <91350928+sprwoo@users.noreply.github.com>
2024-05-14 11:02:06 -0400

    Combined all into one CSV and directly writes into the CSV instead of turning into a DataFrame

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
-import urllib.request
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-from requests_html import HTMLSession
-import pandas as pd
-import re
-import csv
 from scraper import *
 
-for y in range(2019,2024):
-    years = str(y) + "-" + str(y + 1)
-    csvname = years + ".csv"
-    #file = open(csvname, 'a')
-    #file.write("Major,Courses\n")
+with open("test.csv", 'w', newline='', encoding='utf-8') as file:
+    for y in range(2019,2024):
+        years = str(y) + "-" + str(y + 1)
 
-    major_list = get_majors(years)
-    course_requirement_list = []
-    for major in major_list:
-        courses = get_courses(major)
-        course_requirement_list.append(courses)
-    data = {
-        "majors" : major_list,
-        "Courses": course_requirement_list
-    }
-    df = pd.DataFrame(data)
-
-    df.to_csv(csvname, index=False)
+        major_list = get_majors(years)
+        course_requirement_list = []
+        for major in major_list:
+            courses = get_courses(major)
+            file.write(years + "," + str(major) + ",\"" + str(courses) + "\"\n")
+    

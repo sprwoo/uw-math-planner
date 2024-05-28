c82bb5b280b470c8f25f5edc087c2108f3ce3cb6
Ajai Chandi <ajaischandi@gmail.com>
2024-05-27 21:38:30 -0400

    Info change

--- Frontend/schedule.js
+++ Frontend/schedule.js
@@ -50,12 +50,7 @@ document.addEventListener("DOMContentLoaded", function() {
           searchInp = wrapper.querySelector("input"),
           options = wrapper.querySelector(".options");
   
-    let countries = ["Afghanistan", "Algeria", "Argentina", "Australia", "Bangladesh", "Belgium", "Bhutan",
-                     "Brazil", "Canada", "China", "Denmark", "Ethiopia", "Finland", "France", "Germany",
-                     "Hungary", "Iceland", "India", "Indonesia", "Iran", "Italy", "Japan", "Malaysia",
-                     "Maldives", "Mexico", "Morocco", "Nepal", "Netherlands", "Nigeria", "Norway", "Pakistan",
-                     "Peru", "Russia", "Romania", "South Africa", "Spain", "Sri Lanka", "Sweden", "Switzerland",
-                     "Thailand", "Turkey", "Uganda", "Ukraine", "United States", "United Kingdom", "Vietnam"];
+    let countries = ["Placeholder", "TEST"];
   
     function addCountry(selectedCountry) {
         options.innerHTML = "";

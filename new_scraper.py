c0b491a6cbe579db59b9065ac95c81211625dc3e
Daniel Yang <91350928+sprwoo@users.noreply.github.com>
2024-06-06 03:05:45 -0400

    Turned stuff into functions and a bit of documentation

--- new_scraper.py
+++ new_scraper.py
@@ -1,48 +1,54 @@
+from typing import List
 from bs4 import BeautifulSoup
 import requests
 
-# Setup Beautiful Soup
-url = "https://academic-calendar-archive.uwaterloo.ca/undergraduate-studies/2023-2024/page/MATH-Combinatorics-and-Optimization2.html"
-url_page = requests.get(url)
-url_soup = BeautifulSoup(url_page.text, 'lxml')
+def setup_bs(url: str) -> BeautifulSoup:
+    '''Set up BeautifulSoup with the url'''
+    url_page = requests.get(url)
+    url_soup = BeautifulSoup(url_page.text, 'lxml')
+    return url_soup
 
-# Gets the span where all the requirements are listed
-sidebar_span = url_soup.find('span', id='ctl00_contentMain_lblContent')
-lines = sidebar_span.find_all('ul', recursive=False)
+def get_section(soup: BeautifulSoup) -> str:
+    '''Gets the span where all the requirements are listed, then returns all the content as a string of all the requirements'''
+    main_section = soup.find('span', id='ctl00_contentMain_lblContent')
+    lines = main_section.find_all('ul', recursive=False)
+    lines = lines[0].get_text()
+    return lines
 
-# Remove all the tags
-lines = lines[0].get_text()
+def requirement_dict(lines: str) -> dict:
+    '''Takes the string of requirements and turns it into a dictionary with the headers as keys and courses as values'''
+    # Clean up the string and turn it into 
+    requirements = []
+    organized_data = {}
+    valid_keys = ["One", "Two", "Three", "Four", "All", "Elective"] # All of the potential headers
+    count = 1 # Keep track of the numbers
+    string = ""
+    for letter in lines:
+        if letter == '\r' or letter == '\n':
+            if string.strip():
+                if any(valid_key in string for valid_key in valid_keys):
+                    key = f"{count}. {string}"
+                    count += 1
+                    organized_data[key] = []
+                else: # If a key is not found, just append it into the dictionary as a value
+                    if key:
+                        organized_data[key].append(string)
+            string = ""
+        else:
+            string += letter
+        
+    return organized_data
 
-# Get all the requirements into one list
-requirements = []
-string = ""
-for letter in lines:
-    if letter == '\r' or letter == '\n':
-        if string.strip():
-            requirements.append(string.strip())
-        string = ""
-    else:
-        string += letter
+def __repr(data: dict) -> None:
+    '''Prints out a dictionary'''
+    for key, value in organized_data.items():
+        if value:
+            print(f"{key}: {value}")
+        else:
+            print(f"{key}")
+        print("\n")
 
-# Turn the list of requirements into a dictionary 
-organized_data = {}
-valid_keys = ["One", "Two", "Three", "Four", "All", "Elective"] # All of the potential headers
-count = 1 # Keep track of the numbers
-
-for item in requirements:
-    # If any of the keys in valid_keys are found within an element, we create a new dictionary key
-    if any(valid_key in item for valid_key in valid_keys):
-        key = f"{count}. {item}"
-        count += 1
-        organized_data[key] = []
-    else: # If a key is not found, just append it into the dictionary as a value
-        if key:
-            organized_data[key].append(item)
-
-# Print
-for key, value in organized_data.items():
-    if value:
-        print(f"{key}: {value}")
-    else:
-        print(f"{key}")
-    print(">>>")
+soup = setup_bs("https://academic-calendar-archive.uwaterloo.ca/undergraduate-studies/2023-2024/page/MATH-Combinatorics-and-Optimization2.html")
+lines = get_section(soup)
+organized_data = requirement_dict(lines)
+__repr(organized_data)

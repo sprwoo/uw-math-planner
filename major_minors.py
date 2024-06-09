7214bc1bdfa6c2c674a361e3d804264050e78b53
Daniel Yang <91350928+sprwoo@users.noreply.github.com>
2024-06-09 02:26:17 -0400

    scraped into csv, might need to clean up the python code and stuff. I'll do the js converter tomorrow

--- major_minors.py
+++ major_minors.py
@@ -1,4 +1,4 @@
-from scraper import *
+from new_scraper import *
 from typing import Callable
 
 link_header = "https://academic-calendar-archive.uwaterloo.ca"
@@ -6,30 +6,49 @@ link_header = "https://academic-calendar-archive.uwaterloo.ca"
 def scrape(years: int, func: Callable[[int], list[str]]) -> None:
     """
     Wrapper function for datascraping. Generalized so both majors and minors can use this. 
-    Writes into CSV.
+    Writes into a CSV file.
     """
-    years = str(y) + "-" + str(y + 1)
+
+    # Get the years
+    years = str(years) + "-" + str(years + 1)
+
+    # Retrieve all the offered majors/minors for that year
     degree_list = func(years)
+
     for degree in degree_list:
         if degree.get('href') is None:
             continue
-
+        
+        # Get the name of the degree for the CSV
         name = degree.get_text().strip()
         name = name.replace(',', '')
 
+        # I don't wanna deal with how SE's page is set up. Also, it's pretty much all predetermined so no freedom for you SE nerds :(
+        if name == "Software Engineering": 
+            continue
+        
         link = degree.get('href').strip()
 
-        courses = get_courses(degree)
-        if not courses:
-            courses = "Could not find courses associated with this degree :(. Check the official Undergraduate Calendar."
-        file.write(years + "," + name + "," + link_header + link + ",\"" + str(courses) + "\"\n")
+        soup = setup_bs(link_header + link)
+        courses = get_section(soup)
+        organized_data = requirement_dict(courses)
+
+        print(">>>")
+        print(name)
+        print(link_header + link)
+        print(organized_data)
+
+        if not organized_data:
+            organized_data = "Could not find courses associated with this degree :(. Check the official Undergraduate Calendar."
+        file.write(years + "," + name + "," + link_header + link + ",\"" + str(organized_data) + "\"\n")
 
 with open("UW-Undergrad-Calendar/CSVs/course_requirements.csv", 'w', newline='', encoding='utf-8') as file:
     file.write("Year,Offered Major,Link,Course Requirements\n")
-    for y in range(2019,2024):
+    for y in range(2022,2024):
         scrape(y, get_majors)
-    
-with open("UW-Undergrad-Calendar/CSVs/minor_requirements.csv", 'w', newline='', encoding='utf-8') as file:
-    file.write("Year,Offered Minor,Link,Course Requirements\n")
-    for y in range(2019, 2024):
-        scrape(y, get_minors)
+# scrape -> [get_majors -> [setup_bs], setup_bs, get_section, requirement_dict]
+
+# with open("UW-Undergrad-Calendar/CSVs/minor_requirements.csv", 'w', newline='', encoding='utf-8') as file:
+#     file.write("Year,Offered Minor,Link,Course Requirements\n")
+#     for y in range(2019, 2024):
+#         scrape(y, get_minors)
--- new_scraper.py
+++ new_scraper.py
@@ -10,45 +10,82 @@ def setup_bs(url: str) -> BeautifulSoup:
 
 def get_section(soup: BeautifulSoup) -> str:
     '''Gets the span where all the requirements are listed, then returns all the content as a string of all the requirements'''
+    # Get the main section element
     main_section = soup.find('span', id='ctl00_contentMain_lblContent')
-    lines = main_section.find_all('ul', recursive=False)
-    lines = lines[0].get_text()
-    return lines
+
+    # The three html tags the site uses
+    html_tags = ["ul", "ol", "p"]
+
+    # Look for each in order of most common to least
+    for tag in html_tags:
+        if (tag != "p"):
+            lines = main_section.find_all(tag, recursive=False)
+            if len(lines) > 0:
+                return lines[0].get_text()
+        else:
+            # Special case for "p" because sites with <p> is just a lot of yap
+            lines = main_section.find_all(tag, recursive=True)
+            tmp = []
+            for line in lines:
+                tmp.append(line.get_text())
+            return tmp
+        
+    return None
 
 def requirement_dict(lines: str) -> dict:
     '''Takes the string of requirements and turns it into a dictionary with the headers as keys and courses as values'''
-    # Clean up the string and turn it into 
-    requirements = []
     organized_data = {}
-    valid_keys = ["One", "Two", "Three", "Four", "All", "Elective"] # All of the potential headers
+    valid_keys = ["One", "Two", "Three", "Four", "All", "Elective", "Any", "following", 
+                  "minimum", "academic standing", "courses", "offers", "ACTSC 231"] # All of the potential headers (Don't mind that last one, CPA wants to be special)
     count = 1 # Keep track of the numbers
+    key = None
     string = ""
     for letter in lines:
         if letter == '\r' or letter == '\n':
-            if string.strip():
+            string = string.strip()
+            if string:
                 if any(valid_key in string for valid_key in valid_keys):
                     key = f"{count}. {string}"
                     count += 1
                     organized_data[key] = []
                 else: # If a key is not found, just append it into the dictionary as a value
-                    if key:
-                        organized_data[key].append(string)
+                    organized_data[key].append(string)
             string = ""
         else:
             string += letter
-        
+    
+    if string.strip():
+        key = f"{count}. {string}"
+        organized_data[key] = []
     return organized_data
 
-def __repr(data: dict) -> None:
-    '''Prints out a dictionary'''
-    for key, value in organized_data.items():
-        if value:
-            print(f"{key}: {value}")
-        else:
-            print(f"{key}")
-        print("\n")
+def get_majors(year: int) -> list[str]:
+    """Returns all the availiable majors for a year"""
+    # Set up URL
+    url_for_list = "https://academic-calendar-archive.uwaterloo.ca/undergraduate-studies/" + year + "/page/MATH-List-of-Academic-Programs-or-Plans.html"
 
-soup = setup_bs("https://academic-calendar-archive.uwaterloo.ca/undergraduate-studies/2023-2024/page/MATH-Combinatorics-and-Optimization2.html")
-lines = get_section(soup)
-organized_data = requirement_dict(lines)
-__repr(organized_data)
+    # Get all the names
+    major_names = setup_bs(url_for_list)
+    major_names = major_names.find_all('a')
+
+    # Remove everything before "Amendments"
+    i = 0
+    for name in major_names:
+        if "Amendments" in name.get_text():
+            break
+        i += 1
+    major_names = major_names[i + 1:]
+    
+    return major_names
+
+if __name__ == "__main__":
+    soup = setup_bs("https://academic-calendar-archive.uwaterloo.ca/undergraduate-studies/2022-2023/page/ENG-Software-Engineering.html")
+    lines = get_section(soup)
+    #print(lines)
+    if lines:
+        organized_data = requirement_dict(lines)
+    else:
+        organized_data = "No courses"
+    #print(organized_data)
+    for k, v in organized_data.items():
+        print(f"{k}: {v}")

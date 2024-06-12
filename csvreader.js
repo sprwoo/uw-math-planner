86db611d750049ccab3eb77885d45c83f958cc5c
Daniel Yang <91350928+sprwoo@users.noreply.github.com>
2024-06-12 18:53:33 -0400

    Documentation

--- csvreader.js
+++ csvreader.js
@@ -1,26 +1,40 @@
 const csv = require('csv-parser')
 const fs = require('fs')
 
+/**
+ * Parses through course_requirements.csv to find and return an object of the major
+ * @param {String} year         The year of the offerings we are looking for
+ * @param {String} major_name   The name of the major we are looking for
+ * @returns {Promise}           Promise with an object representing the major
+ */
 function lookup_major(year, major_name) {
     return new Promise((resolve, reject) => {
-        const results = [];
-
+        // Parse through the csv
         fs.createReadStream('C:\\Users\\verys\\Documents\\Undergrad Calendar\\UW-Undergrad-Calendar\\CSVs\\course_requirements.csv')
             .pipe(csv())
-            .on('data', (data) => results.push(data))
-            .on('end', () => {
-                for (let i = 0; i < results.length; i++) {
-                    if (results[i].Year == year && results[i].Major == major_name) {
-                        resolve(results[i]);
-                        return;
-                    }
+            .on('data', (data) => {
+                // If a match is found, return the object
+                if (data.Year == year && data.Major == major_name) { 
+                    resolve(data);
+                    return;
                 }
-                reject('No matching requirements found.');
-            })
+            }) 
+
+            // If it reaches the end of the csv, return an error
+            .on('end', () => reject(new Error(`No majors found with the name \"${major_name}\".`)))
+
+            // Error if there was an issue reading csv
             .on('error', (error) => reject(error));
     });
 }
 
+/**
+ * Uses lookup_major to get two objects of two different majors
+ * @param {*} year      The year of the offerings we are looking for
+ * @param {*} name1     The name of the first major we are looking for
+ * @param {*} name2     The name of the second major we are looking for
+ * @returns {void}      Void for printing for now, change to object for frontend
+ */
 async function majors_csv(year, name1, name2) {
     // Replace these with getElementById or something of the sort to grab the user inputs
     year = "2023-2024"
@@ -29,34 +43,50 @@ async function majors_csv(year, name1, name2) {
 
     try {
         const major1 = await lookup_major(year, name1);
-        const major2 = await lookup_major(year, name2);
         console.log(major1);
-        console.log(major2)
+    } catch (error) {
+        console.error(error);
+    }
+
+    try {
+        const major2 = await lookup_major(year, name2);
+        console.log(major2);
     } catch (error) {
         console.error(error);
     }
 }
 
+/**
+ * Parses through course_info.csv to find and return an object representing the course
+ * @param {String} course_code      The code of the course we are looking for
+ * @returns {Promise}               Promise with an object of the course 
+ */
 function lookup_courses(course_code) {
     return new Promise((resolve, reject) => {
-        const results = [];
-
+        // Parse through csv
         fs.createReadStream('C:\\Users\\verys\\Documents\\Undergrad Calendar\\UW-Undergrad-Calendar\\CSVs\\course_info.csv')
             .pipe(csv())
-            .on('data', (data) => results.push(data))
-            .on('end', () => {
-                for (let i = 0; i < results.length; i++) {
-                    if (results[i].Code == course_code) {
-                        resolve(results[i]);
-                        return;
-                    }
+            .on('data', (data) => {
+                // If match is found, return the object
+                if (data.Code == course_code) {
+                    resolve(data);
+                    return;
                 }
-                reject('No matching requirements found.');
             })
+
+            // If it reaches the end of the csv, return an error
+            .on('end', () => reject(new Error(`No courses found with the course code \"${course_code}\".`)))
+
+            // Error if there was an issue reading csv
             .on('error', (error) => reject(error));
     });
 }
 
+/**
+ * Uses lookup_courses to get an object representing the course's information
+ * @param {String} course_code      The code of the course we are looking for
+ * @returns {void}                  Print for testing 
+ */
 async function courses_csv(course_code) {
     course_code = "CO250"
 
@@ -68,5 +98,5 @@ async function courses_csv(course_code) {
     }
 }
 
-//majors_csv();
+majors_csv();
 courses_csv();
--- similaritycheck.js
+++ similaritycheck.js
@@ -1,7 +1,3 @@
-// Parse both req1 and req2 requirements to turn it into a dictionary
-// Iterate through req1 to map each course into its own key, with the major name as a value
-// Then iterate through req2, adding the major name to existing keys
-
 const major1 = {
     Year: '2023-2024',
     Major: 'Joint Pure Mathematics',
@@ -16,10 +12,21 @@ const major2 = {
     Requirements: "{'1. One of': ['MATH 239 Introduction to Combinatorics', 'MATH 249 Introduction to Combinatorics (Advanced Level)'], '2. One of': ['CO 250 Introduction to Optimization', 'CO 255 Introduction to Optimization (Advanced Level)'], '3. One of': ['PMATH 336 Introduction to Group Theory with Applications', 'PMATH 347 Groups and Rings'], '4. Four of': ['CO 330 Combinatorial Enumeration', 'CO 331 Coding Theory', 'CO 342 Introduction to Graph Theory', 'CO 351 Network Flow Theory', 'CO 353 Computational Discrete Optimization', 'CO 367 Nonlinear Optimization', 'CO 430 Algebraic Enumeration', 'CO 431 Symmetric Functions', 'CO 432 Information Theory and Applications', 'CO 434 Combinatorial Designs', 'CO 439 Topics in Combinatorics', 'CO 440 Topics in Graph Theory', 'CO 442 Graph Theory', 'CO 444 Algebraic Graph Theory', 'CO 446 Matroid Theory', 'CO 450 Combinatorial Optimization', 'CO 452 Integer Programming', 'CO 454 Scheduling', 'CO 456 Introduction to Game Theory', 'CO 459 Topics in Optimization', 'CO 463 Convex Optimization and Analysis', 'CO 466 Continuous Optimization', 'CO 471 Semidefinite Optimization', 'CO 481/CS 467/PHYS 467 Introduction to Quantum Information Processing', 'CO 485 The Mathematics of Public-Key Cryptography', 'CO 486 Topics in Quantum Information', 'CO 487 Applied Cryptography'], '5. Three of': ['AMATH 331/PMATH 331 Applied Real Analysis or PMATH 333 Introduction to Real Analysis', 'AMATH 332/PMATH 332 Applied Complex Analysis', 'CS 341 Algorithms', 'CS 360 Introduction to the Theory of Computing', 'CS 466 Algorithm Design and Analysis', 'MATH 237 Calculus 3 for Honours Mathematics or MATH 247 Calculus 3 (Advanced Level)', 'PMATH 334 Introduction to Rings and Fields with Applications or PMATH 348 Fields and Galois Theory', 'PMATH 340 Elementary Number Theory']}"
 }
 
+/** 
+ * Adds to the major's name as a value into the object with the course code as the key
+ * @param {Object} object   The object to add the key and name to
+ * @param {String} key      The name of the course to be added as a key
+ * @param {String} name     The name of the major to be added as a value
+ * @returns {void}
+*/
 function add_to_dict(object, key, name) {
+    // Get rid of whitespaces and single quotes
     key = key.trim();
     key = key.slice(1, -1);
+
+    // Check if the key is a black space
     if (key.length > 0) {
+        // If it alreaady exists, push it into the array, otherwise initalize the array
         if (Array.isArray(object[key])) {
             object[key].push(name);
         } else {
@@ -28,7 +35,13 @@ function add_to_dict(object, key, name) {
     }
 }
 
-function parse_csv_to_obj(major, requirements) {
+/**
+ * Adds the major's requirements to an object
+ * @param  {Object} major   The object representing the major
+ * @param  {Object} object  The object to add the requirements to
+ * @return {void}
+ */
+function parse_csv_to_obj(major, object) {
     let values = false;
     let key = "";
 
@@ -37,11 +50,11 @@ function parse_csv_to_obj(major, requirements) {
         if (ch == '[') {
             values = true;
         } else if (ch == ']') {
-            add_to_dict(requirements, key, major.Major);
+            add_to_dict(object, key, major.Major);
             values = false;
             key = "";
         } else if (ch == ',' && values) {
-            add_to_dict(requirements, key, major.Major);
+            add_to_dict(object, key, major.Major);
             key = "";
         } else if (values) {
             key += ch;

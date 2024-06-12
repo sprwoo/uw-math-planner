c5cd16d72fa8dc9f8cbb047366e55cd024c0a7d2
Daniel Yang <91350928+sprwoo@users.noreply.github.com>
2024-06-12 02:35:51 -0400

    finished the similarity check and csv reader to get specific course information

--- csvreader.js
+++ csvreader.js
@@ -1,7 +1,7 @@
 const csv = require('csv-parser')
 const fs = require('fs')
 
-function get_requirements(lookforyear, lookformajor) {
+function lookup_major(year, major_name) {
     return new Promise((resolve, reject) => {
         const results = [];
 
@@ -10,7 +10,7 @@ function get_requirements(lookforyear, lookformajor) {
             .on('data', (data) => results.push(data))
             .on('end', () => {
                 for (let i = 0; i < results.length; i++) {
-                    if (results[i].Year == lookforyear && results[i].Major == lookformajor) {
+                    if (results[i].Year == year && results[i].Major == major_name) {
                         resolve(results[i]);
                         return;
                     }
@@ -21,20 +21,52 @@ function get_requirements(lookforyear, lookformajor) {
     });
 }
 
-async function main() {
+async function majors_csv(year, name1, name2) {
     // Replace these with getElementById or something of the sort to grab the user inputs
-    const year = "2023-2024"
-    const name1 = "Joint Pure Mathematics"
-    const name2 = "Joint Combinatorics and Optimization"; 
+    year = "2023-2024"
+    name1 = "Joint Pure Mathematics"
+    name2 = "Joint Combinatorics and Optimization"; 
 
     try {
-        const major1 = await get_requirements(year, name1);
-        const major2 = await get_requirements(year, name2);
-        console.log(req1);
-        console.log(req2)
+        const major1 = await lookup_major(year, name1);
+        const major2 = await lookup_major(year, name2);
+        console.log(major1);
+        console.log(major2)
     } catch (error) {
         console.error(error);
     }
 }
 
-main();
+function lookup_courses(course_code) {
+    return new Promise((resolve, reject) => {
+        const results = [];
+
+        fs.createReadStream('C:\\Users\\verys\\Documents\\Undergrad Calendar\\UW-Undergrad-Calendar\\CSVs\\course_info.csv')
+            .pipe(csv())
+            .on('data', (data) => results.push(data))
+            .on('end', () => {
+                for (let i = 0; i < results.length; i++) {
+                    if (results[i].Code == course_code) {
+                        resolve(results[i]);
+                        return;
+                    }
+                }
+                reject('No matching requirements found.');
+            })
+            .on('error', (error) => reject(error));
+    });
+}
+
+async function courses_csv(course_code) {
+    course_code = "CO250"
+
+    try {
+        const course_info = await lookup_courses(course_code);
+        console.log(course_info);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+//majors_csv();
+courses_csv();
--- similaritycheck.js
+++ similaritycheck.js
@@ -16,32 +16,47 @@ const major2 = {
     Requirements: "{'1. One of': ['MATH 239 Introduction to Combinatorics', 'MATH 249 Introduction to Combinatorics (Advanced Level)'], '2. One of': ['CO 250 Introduction to Optimization', 'CO 255 Introduction to Optimization (Advanced Level)'], '3. One of': ['PMATH 336 Introduction to Group Theory with Applications', 'PMATH 347 Groups and Rings'], '4. Four of': ['CO 330 Combinatorial Enumeration', 'CO 331 Coding Theory', 'CO 342 Introduction to Graph Theory', 'CO 351 Network Flow Theory', 'CO 353 Computational Discrete Optimization', 'CO 367 Nonlinear Optimization', 'CO 430 Algebraic Enumeration', 'CO 431 Symmetric Functions', 'CO 432 Information Theory and Applications', 'CO 434 Combinatorial Designs', 'CO 439 Topics in Combinatorics', 'CO 440 Topics in Graph Theory', 'CO 442 Graph Theory', 'CO 444 Algebraic Graph Theory', 'CO 446 Matroid Theory', 'CO 450 Combinatorial Optimization', 'CO 452 Integer Programming', 'CO 454 Scheduling', 'CO 456 Introduction to Game Theory', 'CO 459 Topics in Optimization', 'CO 463 Convex Optimization and Analysis', 'CO 466 Continuous Optimization', 'CO 471 Semidefinite Optimization', 'CO 481/CS 467/PHYS 467 Introduction to Quantum Information Processing', 'CO 485 The Mathematics of Public-Key Cryptography', 'CO 486 Topics in Quantum Information', 'CO 487 Applied Cryptography'], '5. Three of': ['AMATH 331/PMATH 331 Applied Real Analysis or PMATH 333 Introduction to Real Analysis', 'AMATH 332/PMATH 332 Applied Complex Analysis', 'CS 341 Algorithms', 'CS 360 Introduction to the Theory of Computing', 'CS 466 Algorithm Design and Analysis', 'MATH 237 Calculus 3 for Honours Mathematics or MATH 247 Calculus 3 (Advanced Level)', 'PMATH 334 Introduction to Rings and Fields with Applications or PMATH 348 Fields and Galois Theory', 'PMATH 340 Elementary Number Theory']}"
 }
 
-let req1 = {};
-let values = false;
-let key = "";
-
-function add_to_dict(key, major) {
+function add_to_dict(object, key, name) {
     key = key.trim();
     key = key.slice(1, -1);
     if (key.length > 0) {
-        req1[key] = major;
+        if (Array.isArray(object[key])) {
+            object[key].push(name);
+        } else {
+            object[key] = [name];
+        }
     }
 }
 
-for (let i = 0; i < major1.Requirements.length; i++) {
-    let ch = major1.Requirements[i];
-    if (ch == '[') {
-        values = true;
-    } else if (ch == ']') {
-        add_to_dict(key, major1.Major);
-        values = false;
-        key = "";
-    } else if (ch == ',' && values) {
-        add_to_dict(key, major1.Major);
-        key = "";
-    } else if (values) {
-        key += ch;
-    } 
+function parse_csv_to_obj(major, requirements) {
+    let values = false;
+    let key = "";
+
+    for (let i = 0; i < major.Requirements.length; i++) {
+        let ch = major.Requirements[i];
+        if (ch == '[') {
+            values = true;
+        } else if (ch == ']') {
+            add_to_dict(requirements, key, major.Major);
+            values = false;
+            key = "";
+        } else if (ch == ',' && values) {
+            add_to_dict(requirements, key, major.Major);
+            key = "";
+        } else if (values) {
+            key += ch;
+        } 
+    }
 }
 
-console.log(req1);
+var requirements = {};
+parse_csv_to_obj(major1, requirements);
+parse_csv_to_obj(major2, requirements);
+
+for (let key in requirements) {
+    if (requirements[key].length < 2) {
+        delete requirements[key];
+    }
+}
+
+console.log(requirements)

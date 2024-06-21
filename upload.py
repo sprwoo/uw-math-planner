d2f52245e1d195a9cfa03433951556b01dbfe883
Daniel Yang <91350928+sprwoo@users.noreply.github.com>
2024-06-21 19:44:04 -0400

    i hate postgresql

--- upload.py
+++ upload.py
@@ -1,4 +1,7 @@
 import csv
+import psycopg2
+import os
+from dotenv import load_dotenv, dotenv_values
 
 # Had to change the orders
 # with open('CSVs/course_requirements.csv') as file:
@@ -19,7 +22,42 @@ import csv
 #     for row in data:
 #         csv_output.writerow([row[1], row[0], row[2], row[3]])
 
-with open('CSVs/course_requirements.csv') as file:
-    data = list(csv.reader(file))
-    for x in data:
-        print(x)
+load_dotenv()
+
+# RDS connection details
+rds_host = os.getenv("host")
+rds_data = os.getenv("db")
+rds_user = os.getenv("user")
+rds_password = os.getenv("password")
+
+try :
+    connect = psycopg2.connect(
+        host = rds_host,
+        database = rds_data,
+        user = rds_user,
+        password = rds_password,
+        port = 5432,
+    )
+    cursor = connect.cursor
+    print("Connected!")
+
+    connect.close()
+except Exception as e:
+    print(f"Failed! {e}")
+
+# insert_query = "INSERT INTO table (name, years, link, requirements) VALUES (%s, %s, %s, %s)"
+# cursor.execute(insert_query, 
+#                r"Actuarial Science,2022-2023,\
+#                https://academic-calendar-archive.uwaterloo.ca/undergraduate-studies/2022-2023/page/MATH-Actuarial-Science1.html, things")
+
+# connect.commit()
+# print("Success!")
+
+# cursor.close()
+# connect.close()
+
+# with open('CSVs/course_requirements.csv') as file:
+#     data = list(csv.reader(file))[1:]
+#     for x in data:
+#         insert_query = "INSERT INTO table (name, years, link, requirements) VALUES (%s, %s, %s, %s)"
+    

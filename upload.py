cfe36415f68957ab6af32850cc3a2ad3e9369ed9
Daniel Yang <91350928+sprwoo@users.noreply.github.com>
2024-06-24 23:07:05 -0400

    added to db

--- upload.py
+++ upload.py
@@ -3,25 +3,6 @@ import psycopg2
 import os
 from dotenv import load_dotenv, dotenv_values
 
-# Had to change the orders
-# with open('CSVs/course_requirements.csv') as file:
-#     data = list(csv.reader(file))
-    
-# with open('CSVs/course_requirements.csv', 'w', newline='') as file:
-#     csv_output = csv.writer(file)
-    
-#     for row in data:
-#         csv_output.writerow([row[1], row[0], row[2], row[3]])
-
-# with open('CSVs/minor_requirements.csv') as file:
-#     data = list(csv.reader(file))
-    
-# with open('CSVs/minor_requirements.csv', 'w', newline='') as file:
-#     csv_output = csv.writer(file)
-    
-#     for row in data:
-#         csv_output.writerow([row[1], row[0], row[2], row[3]])
-
 load_dotenv()
 
 # RDS connection details
@@ -30,7 +11,8 @@ rds_data = os.getenv("db")
 rds_user = os.getenv("user")
 rds_password = os.getenv("password")
 
-try :
+try:
+    # Connect to the PostgreSQL server
     connect = psycopg2.connect(
         host = rds_host,
         database = rds_data,
@@ -43,31 +25,23 @@ try :
     cursor = connect.cursor()
     print("Connected!")
 
-    table_create = """ create table customerInfo(id SERIAL PRIMARY KEY, name text, age float )   """
+    # Reset the table
+    cursor.execute("DROP TABLE IF EXISTS major_courses")
+    table_create = """ create table major_courses(id SERIAL PRIMARY KEY, Major text, Year text, Link text, Requirements text)   """
     cursor.execute(table_create)
 
-    insert_query = """insert into customerInfo (name, age) values ('abc', 22)  """
-    cursor.execute(insert_query)
+    # Open the CSV and begin writing to database
+    with open("CSVs/course_requirements.csv", 'r', newline='', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            insert_query = "INSERT INTO major_courses (Major, Year, Link, Requirements) VALUES (%s, %s, %s, %s)"
+            print(row["Major"], row["Year"], row["Link"], row["Requirements"])
+            cursor.execute(insert_query, (row["Major"], row["Year"], row["Link"], row["Requirements"]))
     print("DONE!")
 
+    # Close connections
+    cursor.close()
     connect.close()
 
 except Exception as e:
     print(f"Failed! {e}")
-
-# insert_query = "INSERT INTO table (name, years, link, requirements) VALUES (%s, %s, %s, %s)"
-# cursor.execute(insert_query, 
-#                r"Actuarial Science,2022-2023,\
-#                https://academic-calendar-archive.uwaterloo.ca/undergraduate-studies/2022-2023/page/MATH-Actuarial-Science1.html, things")
-
-# connect.commit()
-# print("Success!")
-
-# cursor.close()
-# connect.close()
-
-# with open('CSVs/course_requirements.csv') as file:
-#     data = list(csv.reader(file))[1:]
-#     for x in data:
-#         insert_query = "INSERT INTO table (name, years, link, requirements) VALUES (%s, %s, %s, %s)"
-    

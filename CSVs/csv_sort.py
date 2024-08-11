8a2bfbb8c40ff4692214861e9f009a15deb3da48
Daniel Yang <91350928+sprwoo@users.noreply.github.com>
2024-08-11 19:05:35 -0400

    fixed sorting

--- CSVs/csv_sort.py
+++ CSVs/csv_sort.py
@@ -5,16 +5,17 @@ def sort_csv(input_file, output_file):
     with open(input_file, mode='r', newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
         header = next(reader)  
-        sorted_rows = sorted(reader, key=lambda row: row[0])  
+        sorted1 = sorted(reader, key=lambda row: row[1])
+        sorted2 = sorted(sorted1, key=lambda row: row[0])
     
     # Write the sorted rows to a new CSV file
     with open(output_file, mode='w', newline='', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(header)  
-        writer.writerows(sorted_rows)  
+        writer.writerows(sorted2)  
 
-input_file = 'CSVs/other_minors.csv'
-output_file = 'CSVs/sorted_other_minors.csv'
+input_file = 'CSVs/major_requirements.csv'
+output_file = 'CSVs/major_requirements.csv'
 sort_csv(input_file, output_file)
 
 print("Done!")
